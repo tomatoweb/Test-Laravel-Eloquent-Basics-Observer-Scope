d4f96506e96b34f1d660b91f3c2f5e80913f6b40
Mathias Appelmans <math.appelmans@gmail.com>
2025-03-28 13:58:25 +0100

    added response code to the features phpunit tests

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        // TASK: turn this SQL query into Eloquent
+        // select * from users
+        //   where email_verified_at is not null
+        //   order by created_at desc
+        //   limit 3
+
+        //$users = User::all(); // replace this with Eloquent statement
+        $users = User::whereNotNull('email_verified_at')->orderBy('created_at', 'DESC')->limit(3)->get();
+
+
+        return view('users.index', compact('users'));
+    }
+
+    public function show($userId)
+    {
+        //$user = NULL; // TASK: find user by $userId or show "404 not found" page
+        $user = User::findOrFail($userId);
+
+        return view('users.show', compact('user'));
+    }
+
+    public function check_create($name, $email)
+    {
+        // TASK: find a user by $name and $email
+        //   if not found, create a user with $name, $email and random password
+        //$user = NULL;
+        $user = User::firstOrCreate([
+            'name' => $name,
+            'email' => $email
+        ],
+        [   
+            'password' => Hash::make('random_password')
+        ]
+    );
+
+        return view('users.show', compact('user'));
+    }
+
+    public function check_update($name, $email)
+    {
+        // TASK: find a user by $name and update it with $email
+        //   if not found, create a user with $name, $email and random password
+        // $user = NULL; // updated or created user
+        $data = [
+            'name' => $name            
+        ];
+        $values = [
+            'email' => $email,
+            'password' => 'password'
+        ];
+        $user = User::updateOrCreate($data, $values);
+
+        return view('users.show', compact('user'));
+    }
+
+    public function destroy(Request $request)
+    {
+        // TASK: delete multiple users by their IDs
+        // SQL: delete from users where id in ($request->users)
+        // $request->users is an array of IDs, ex. [1, 2, 3]
+
+        // Insert Eloquent statement here
+        $deleted = User::whereIn('id',  $request->users)->delete();
+
+        return redirect('/')->with('success', 'Users deleted');
+    }
+
+    public function only_active()
+    {
+        // TASK: That "active()" doesn't exist at the moment.
+        //   Create this scope to filter "where email_verified_at is not null"
+        $users = User::active()->get();
+
+        return view('users.index', compact('users'));
+    }
+
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Project;
+use App\Observers\ProjectObserver;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+
+        Project::observe(ProjectObserver::class);
+    }
+}
